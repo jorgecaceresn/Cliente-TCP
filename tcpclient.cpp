6094cd7be74e84f4c9779b64a01a2f61b9732614
jorgecaceresn <64871994+jorgecaceresn@users.noreply.github.com>
2021-05-28 19:53:23 -0500

    finished proyect

--- tcpclient.cpp
+++ tcpclient.cpp
@@ -1,15 +1,62 @@
 #include "tcpclient.h"
 #include "ui_tcpclient.h"
 
-TcpClient::TcpClient(QWidget *parent)
-    : QDialog(parent)
-    , ui(new Ui::TcpClient)
+TcpClient::TcpClient(QWidget *parent): QDialog(parent), ui(new Ui::TcpClient)
 {
     ui->setupUi(this);
+    qTcpSocket = new QTcpSocket(this);
+    in.setDevice(qTcpSocket);
+    //connect push button to quit
+    connect(ui->btn_close,&QAbstractButton::clicked, this, &QWidget::close);
+    //connect request message
+    connect(ui->btn_request,&QAbstractButton::clicked,this,&TcpClient::requestMessage);
+    //conect
+    connect(qTcpSocket,&QIODevice::readyRead,this,&TcpClient::readMessage);
+    connect(qTcpSocket,&QAbstractSocket::errorOccurred,this,&TcpClient::showError);
 }
 
 TcpClient::~TcpClient()
 {
     delete ui;
+}
+
+void TcpClient::requestMessage()
+{
+    qInfo()<<"request Message";
+    QString ipAddress = ui->le_ip->text();
+    QString port = ui->le_port->text();
+    qTcpSocket->connectToHost(ipAddress,port.toInt());
+    qInfo()<<"ip: "+ ipAddress+", port: "+port;
+}
+
+void TcpClient::readMessage()
+{
+    qInfo()<<"read Message";
+    in.startTransaction();
+    QString message;
+    in>> message;
+    if (!in.commitTransaction()){
+        qInfo()<<"Error in datastream";
+        return;
+    }
+    ui->lbl_message->setText(message);
+}
+
+void TcpClient::showError(QAbstractSocket::SocketError socketError)
+{
+    qInfo()<<"show Error";
+    switch(socketError){
+    case 0:
+        qInfo()<<"Conexión rechazada";
+        break;
+    case 1:
+        qInfo() <<"El servidor cerró la conexión";
+        break;
+    default:
+        qInfo() <<"Error no detectado";
+
+    }
+
+
 }
 
--- tcpclient.h
+++ tcpclient.h
@@ -2,7 +2,7 @@
 #define TCPCLIENT_H
 
 #include <QDialog>
-
+#include <QTcpSocket>
 QT_BEGIN_NAMESPACE
 namespace Ui { class TcpClient; }
 QT_END_NAMESPACE
@@ -15,7 +15,14 @@ public:
     TcpClient(QWidget *parent = nullptr);
     ~TcpClient();
 
+private slots:
+    void requestMessage();
+    void readMessage();
+    void showError(QAbstractSocket::SocketError socketError);
 private:
     Ui::TcpClient *ui;
+    QTcpSocket *qTcpSocket = nullptr;
+    QDataStream in;
+    QString receivedMessage;
 };
 #endif // TCPCLIENT_H
